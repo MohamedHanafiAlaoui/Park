d33d0cf26a33607000165b4f05eba11f753c4c64
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-03-14 19:49:44 +0000

    ajouter fonctions register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
 
-    public function register()
+    public function register(Request $request)
     {
+        $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|max:255|unique:users,email',
+            'password'=>'required|string|min:8|confirmed',
+        ]);
+
+       $user= User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+       ]);
+       return response()->json(['message'=>'user registered Succeccfully','user'=>$user],201);
 
     }
 
-    public function login()
+    public function login(request $request)
     {
-
+        $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|string',
+        ]);
+        if (!Auth) {
+            # code...
+        }
     }
 
     public function logout()
